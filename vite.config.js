89b466a76489891c97140246b8a0e00ed9647fe5
小航同学 <3418395803@qq.com>
2024-01-16 10:23:02 +0800

    level1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'  
 import vue from '@vitejs/plugin-vue'  
-  
+
 export default defineConfig({ 
   base: "/Steam/",
   build: {
@@ -11,14 +11,8 @@ export default defineConfig({
   ],  
   resolve: {  
     alias: {  
-      '@': '/src' // 这里根据你的项目结构进行修改  
+      '@': '/src'
     }  
   },  
-  cors: {  
-    // 在这里添加 CORS 配置  
-    // 例如：允许来自特定源的请求  
-    allowedOrigins: ['http://localhost:5173'],  
-    // 其他 CORS 配置项...  
-  }  
 }
 )
